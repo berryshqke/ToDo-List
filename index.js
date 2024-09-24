f72d2024468e5430a5151485562f28fc18b0ea05
Jasper Yang <jyan457@uwo.ca>
2024-09-23 22:45:51 -0400

    finished render, add, and delete functions

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-let items = ["task1", "task2"];
-const itemsDiv = document.getElementById("items")
+let items = [];
+const input = document.getElementById("itemInput");
+const itemsDiv = document.getElementById("items");
 
 function loadItems(){
+    
+}
+
+function renderItems(){
     itemsDiv.innerHTML = null;
     for(const[idx, item] of Object.entries(items)){
-        const container = document.createElement("div");
-        //container.style.marginBottom = "10vh";
+        const container = document.createElement("p");
+        container.style.marginBottom = "2vh";
 
-        const text = document.createElement("p");
+        const text = document.createElement("span");
         text.textContent = item;
 
         const button = document.createElement("button");
@@ -21,20 +26,22 @@ function loadItems(){
     }
 }
 
-function renderItems(){
-    loadItems();
-}
-
 function saveItems(){
 
 }
 
 function addItem(){
-
+    const value = input.value;
+    if(!value){
+        alert("item is empty");
+        return;
+    }
+    items.push(value);
+    renderItems();
+    input.value = "";
 }
 
-function removeItem(){
-
+function removeItem(idx){
+    items.splice(idx, 1);
+    renderItems();
 }
-
-renderItems();
